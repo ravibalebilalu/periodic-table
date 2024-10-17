8c8519d28d11a807352769417c7c1afe63b1e1ad
ravibalebilalu <81ravikiran@gmail.com>
2024-10-17 20:54:49 +0530

    add styles to Element

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -1,57 +1,70 @@
-import React from 'react';
+import './element.css';
 
-function Element({ elementsData, elementName ,handleShowTable,showTable}) {
+function Element({ elementsData, elementName, handleShowTable, showTable }) {
     const element = elementsData.elements.filter(
         (el) => el.name === elementName
     );
 
     return (
-        <div className={`element ${showTable?'hidden':''}`}>
+        <div className={`element ${showTable ? 'hidden' : ''}`}>
             <button onClick={handleShowTable}>Table</button>
             {element.map((el) => (
                 <div key={el.number}>
-                    <p>{el.name}</p>
-                     
-                    <p>{el.appearance  }</p>
-                    <p> {el.atomic_mass} </p>  
-                    <p>{el.boil  } </p>
-                    <p> {el.category}   </p>
-                    <p> {el.density } </p> 
+                      <p className="elname">{el.name}</p>
+                    <div className="firstrow">
+                        <div className="firstdiv">
+                          
+                            <p className="appearance">
+                                <b>Appearance:</b> {el.appearance}
+                            </p>
+                            <p className="atomic_mass">
+                                <b>Atomic Mass:</b>
+                                {el.atomic_mass}
+                            </p>
+                            <p className="boil">
+                                <b>Boiling Point:</b>
+                                {el.boil}
+                            </p>
+                            <p className="category">
+                                <b>Category:</b> {el.category}{' '}
+                            </p>
+                            <p className="density">
+                                <b>Density : </b>
+                                {el.density}
+                            </p>
+                        </div>
 
-                    <p>  {el.discovered_by} </p>  
-                    <p>{el.melt}</p>  
-                    <p>{el.molar_heat}</p>  
-                    <p>{el.named_by}</p>  
-                    <p>{el.number}</p>  
-                    <p>{el.period }</p> 
+                        <div className="seconddiv">
+                            <p> {el.discovered_by} </p>
+                            <p>{el.melt}</p>
+                            <p>{el.molar_heat}</p>
+                            <p>{el.named_by}</p>
+                            <p>{el.number}</p>
+                            <p>{el.period}</p>
+                        </div>
+                    </div>
 
-                    <p>{el.group} </p>  
-                    <p>{el.phase}</p>  
-                    <a href={el.source }></a> 
-                    <img src={el.bohr_model_image} alt={el.name} />  
-                     
+                    <p>{el.group} </p>
+                    <p>{el.phase}</p>
+                    <a href={el.source}></a>
+                    <img src={el.bohr_model_image} alt={el.name} />
 
-                     
-                    <p>{el.summary  }</p>
-                    <p>{el.symbol}</p>  
-                    <p>{el.xpos  }</p>
-                    <p>{el.ypos}</p>  
-                    <p>{el.wxpos }</p> 
-                    <p>{el.wypos}</p>  
- 
-                    <p>{el.shells  }</p>
-                    <p>{el.electron_configuration }</p> 
-                    <p>{el.electron_configuration_semantic }</p> 
- 
-                    <p>{el.electron_affinity  }</p>
-                    <p>{el.electronegativity_pauling  }</p>
-                    <p>{el.ionization_energies }</p> 
- 
-                     
-                    <p>{el.block}</p>  
-                    
-                     
-                     
+                    <p>{el.summary}</p>
+                    <p>{el.symbol}</p>
+                    <p>{el.xpos}</p>
+                    <p>{el.ypos}</p>
+                    <p>{el.wxpos}</p>
+                    <p>{el.wypos}</p>
+
+                    <p>{el.shells}</p>
+                    <p>{el.electron_configuration}</p>
+                    <p>{el.electron_configuration_semantic}</p>
+
+                    <p>{el.electron_affinity}</p>
+                    <p>{el.electronegativity_pauling}</p>
+                    <p>{el.ionization_energies}</p>
+
+                    <p>{el.block}</p>
                 </div>
             ))}
         </div>
